de06c131b175370cc221e86656e809c2bb021744
Renato Laranjo <renatolaranjo@gmail.com>
2019-01-28 19:21:14 -0200

    First commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "-- Let's start our provision --"
+sudo apt-add-repository ppa:ondrej/php
+sudo apt-get update
+echo "-- Install PHP --"
+sudo apt-get install -y php7.2 libapache2-mod-php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-mysql php7.2-zip
+php -v
+echo "-- Installing Composer --"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/bin/composer
+echo "-- Installing PHP CS --"
+composer global require friendsofphp/php-cs-fixer
+
+	
